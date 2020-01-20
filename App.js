a8114a07f937a4b7978d1957c4ab89c738347dae
谷口 <semikoron.koron@gmail.com>
2020-01-20 18:48:18 +0900

    Create Login and Signup screen

--- App.js
+++ App.js
@@ -2,15 +2,17 @@ import React from 'react';
 import { StyleSheet, View } from 'react-native';
 
 import Appbar from './src/components/Appbar';
-// import MemoListScreen from './src/screens/MemoListScreen'
-// import MemoListScreen from './src/screens/MemoDetailScreen'
-import MemoListScreen from './src/screens/MemoEditScreen'
+// import MemoListScreen from './src/screens/MemoListScreen';
+// import MemoListScreen from './src/screens/MemoDetailScreen';
+// import MemoListScreen from './src/screens/MemoEditScreen';
+import LoginScreen from './src/screens/SignupScreen';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Appbar />
-      <MemoListScreen />
+      {/* <MemoListScreen /> */}
+      <LoginScreen />
     </View>
   );
 }
